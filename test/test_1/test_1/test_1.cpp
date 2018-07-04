12f8447a8c3ee5d25030b7c8fa619012f0479a1b
carlosalves24 <carlos_ama24@live.com.pt>
2018-07-04 22:52:40 +0100

    first commit

---
+++ test/test_1/test_1/test_1.cpp
@@ -0,0 +1,17 @@
+// test_1.cpp : Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{ 
+	cout << "Hello World "<< endl;
+	system("pause");
+    return 0;
+
+}
+
